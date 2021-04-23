0c1e15ca00432e9a8d5c3956484bfd0eb803ae99
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-24 01:32:34 +0200

    initial commit

---
+++ Arrays, Objects, Loops/script.js
@@ -0,0 +1,13 @@
+// Ternary Operator
+// Shorter version of an if statement, it returns also a value
+
+
+x = 10
+let ternaryOperator = x > 10 ? true : false;
+
+// the same as with if statemen example
+if (x > 10){
+    ternaryOperator = true
+}else{
+    ternaryOperator = false
+}
---
+++ Data Types/script.js
@@ -0,0 +1,43 @@
+// an ALGHORITM is a finite set of steps for solving a particular problem
+
+// DATA TYPES
+
+// NUMBERS
+// 5
+// 5.5
+// 45.24545654
+
+// STRINGS
+// "hello strivers"
+// 'hello strivers'
+// `hello strivers`
+
+
+// BOOLEANS
+// true
+// false
+// 3 > 2 --> true
+// 4 > 10 --> false
+// 3 === 3 --> true
+// 10 === 5 --> false
+// 5 === '5' --> false
+// 'strive' === 'strive' --> true
+// === --> checks if they are the same in VALUE and TYPE
+// 5 == '5' --> true DO NOT USE THIS
+// '' --> false
+// 'nando' --> true 
+
+// SPECIAL ONES
+// undefined -> the value is not there yet
+// null -> not existing, the total absence of value
+
+// VARIABLES
+let firstValue = 10 // this is a variable creation, because I used let
+// single = means ASSIGNMENT
+// now firstValue is 10
+
+
+console.log('!!!', firstValue)
+
+// && --> AND
+// || --> OR
