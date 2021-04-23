1bb904c6d418e7bbebe6337e2f030defa6884976
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-24 01:38:05 +0200

    ternary operator

--- Arrays, Objects, Loops/script.js
+++ Arrays, Objects, Loops/script.js
@@ -1,7 +1,12 @@
 // Ternary Operator
 // Shorter version of an if statement, it returns also a value
 
+const age = 19;
 
+// if age is greater than 18 price will hold "20$" as a value, otherwise it will hold "15$"	
+const price = age > 18 ? "20$" : "15$"; 
+
+// simple example
 x = 10
 let ternaryOperator = x > 10 ? true : false;
 
