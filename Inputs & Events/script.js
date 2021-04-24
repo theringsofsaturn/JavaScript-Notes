ccfd9885145b3261bfe375834a3b5b5dafb586a5
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-24 16:25:18 +0200

    Define event listeners in HTML

--- Inputs & Events/script.js
+++ Inputs & Events/script.js
@@ -8,3 +8,14 @@
 
 let myInput = document.getElementById('firstInput')
 console.log(myInput.value) // OUTPUTS THE INPUT FIELD'S VALUE
+
+
+
+// Define event listeners in HTML
+
+<button onclick="myFunc(event)">CLICK ME</button>
+
+// then you declare the function in JS
+const myFunc = function(e) {
+    alert('You clicked me')
+}
