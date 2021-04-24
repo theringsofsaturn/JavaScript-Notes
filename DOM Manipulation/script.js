7bbe5e7edd72f330d308c434c86e64358021dd0b
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-24 15:21:11 +0200

    getElementsByClassName

--- DOM Manipulation/script.js
+++ DOM Manipulation/script.js
@@ -13,3 +13,15 @@ document.getElementById('myButton') // example
 
 // It will always return at most ONE reference to ONE element in the page.
 
+
+// getElementsByClassName
+// If some elements do have a common class property, you can select them with:
+
+
+
+document.getElementsByClassName('putTheClassHere')
+
+document.getElementsByClassName('row') // example‌
+
+
+// It will return an array of elements which have the selected class (even if just one element is found).
