aa579d9e4cb5a460df7c40e0ca8c08cc22298638
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-24 15:03:59 +0200

    Template literals

---
+++ Template Literals/script.js
@@ -0,0 +1,21 @@
+// Template Literals
+// Remember this? 
+
+console.log("Hello Strivers I am a string!");
+console.log('Hello Strivers I am a string too!');
+console.log(`Hello Strivers I have backticks but I am a string too!`);
+
+
+// We said that strings in JS can be declared also with backticks --> `
+
+// They are called template literals.
+
+
+
+// With template literals you can 'break' strings into multiple lines. 
+// You can also insert variables and expressions using the ${ } syntax. 
+// They are a more modern and handy way to interact with text in JS
+
+const name = "Tom"
+const email = `Dear ${name},
+				since I’m using template literals, I can go to a new line`
