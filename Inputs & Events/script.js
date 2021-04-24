3305014f1c688bfb77f3667dde8ce77726516130
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-24 16:08:53 +0200

    Accessing input fields

---
+++ Inputs & Events/script.js
@@ -0,0 +1,10 @@
+// Accessing input fields
+
+
+{/* <input> tags are used for user interaction. 
+You can always read/overwrite their content accessing the value property of their JavaScript reference. */}
+
+
+
+let myInput = document.getElementById('firstInput')
+console.log(myInput.value) // OUTPUTS THE INPUT FIELD'S VALUE
