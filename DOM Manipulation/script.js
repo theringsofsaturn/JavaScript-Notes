acbe8d473b70c9b7936e21e52f9834fa901566ba
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-24 15:20:20 +0200

    DOM Manipulation

---
+++ DOM Manipulation/script.js
@@ -0,0 +1,16 @@
+// Document methods for selecting HTML elements
+
+
+// getElementById
+
+// If the element has a unique id property, you can select it with:
+
+
+
+document.getElementById('putTheIdHere')
+
+document.getElementById('myButton') // example
+
+
+// It will always return at most ONE reference to ONE element in the page.
+
