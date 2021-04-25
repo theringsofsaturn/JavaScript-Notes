e8f46ace8548097208d28d5c3adcec7d58b05719
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-25 23:46:48 +0200

    Date Object methods, DOM Manipulation, Working with Arrays

---
+++ Date Object methods, DOM Manipulation, Working with Arrays/script.js
@@ -0,0 +1,22 @@
+Date Object methods, DOM Manipulation, Working with Arrays
+Not completed yet
+How to use Date objects in Javascript:
+MDN reference: https://developer.mozilla.org/it/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+
+
+the way in which we can extract the current date and time is by invoking the Date() constructor:
+
+let now = new Date();
+
+
+we also have methods to use with Date objects such as: 
+
+now.getFullYear();
+now.getMonth();
+now.getDate();
+that will return respectively:
+
+year number (eg. 1970)
+month number (from 0 to 11), I know, very strange indeed!
+the day number (from 1 to 31)
