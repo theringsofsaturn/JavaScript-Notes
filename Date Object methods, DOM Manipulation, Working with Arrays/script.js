cc1849009ce8d2865c8bebb73de5ee10b1ae6178
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-25 23:54:08 +0200

    Update script.js

--- Date Object methods, DOM Manipulation, Working with Arrays/script.js
+++ Date Object methods, DOM Manipulation, Working with Arrays/script.js
@@ -15,8 +15,45 @@ let now = new Date();
 now.getFullYear();
 now.getMonth();
 now.getDate();
-that will return respectively:
+// that will return respectively:
 
 // year number (eg. 1970)
 // month number (from 0 to 11), I know, very strange indeed!
 // the day number (from 1 to 31)
+
+
+// Grabbing the total number of days in the current month
+// In the project we are using a trick to extract dynamically the exact number of days in the current month by using this code:
+
+new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate()
+
+
+// so you can use parameters in the Date() function to create a date object for a different date/time in the past or the future.
+
+
+
+// by using:
+
+new Date(2021, 11, 0)
+// we are asking to create a date string with 2021 as a year, 11 as December, and with 0 we are asking to go back 1 day from that month 1st day.
+
+
+
+// that's why we get back this:
+
+// Tue Nov 30 2021 00:00:00 GMT+0100
+// November 30th 2021. 
+
+// And that's the trick to get the total number of days of a particular month. 
+
+// You'd use now.getMonth() + 1 to grab the next month, just to go back one day and get the last day of the previous month, hence the number of days in the current month!
+
+
+
+// of course we could use it regularly to just grab any particular day like:
+
+new Date(2021, 11, 25)
+// for Christmas 2021:
+
+// Sat Dec 25 2021 00:00:00 GMT+0100
+
